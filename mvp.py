1f34a575992802946c30bb46a030ac3f5a7579ff
prikmmo9 <109761180+prikmmo9@users.noreply.github.com>
2026-05-04 22:59:59 +0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -13,9 +13,9 @@ from logger_config import create_logger
 # ============================================================
 # КОНФИГУРАЦИЯ
 # ============================================================
-BOT_TOKEN=8566350925:AAEOwpPgXhmR3SE_7TapSbzMJnqImnMA-Js
-API_ID=22268845
-API_HASH=ffbeffdfb86784e12b39aea5f53857d2
+ADMIN_TOKEN = os.getenv('ADMIN_TOKEN', '000')
+API_ID = int(os.getenv('API_ID', '1234567'))
+API_HASH = os.getenv('API_HASH', 'your_api_hash_here')
 
 DOWNLOAD_FOLDER = 'downloads'
 MAX_FILE_SIZE_MB = 2000
