269ee75bb9e984c043ac04e24d5bad41741d341b
prikmmo9 <109761180+prikmmo9@users.noreply.github.com>
2026-05-07 06:20:13 +0300

    Update bot5.py

--- bot5.py
+++ bot5.py
@@ -889,7 +889,7 @@ if __name__ == '__main__':
         print(f"❌ Установите: pip install yt-dlp telethon requests")
         exit(1)
     
-    client.loop.run_until_complete(main())    name='MediaBot',
+    client.loop.run_until_complete(main()    name='MediaBot',
     level=logging.DEBUG,
     detailed=True,
     show_separators=True
